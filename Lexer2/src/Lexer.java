16ecde429c4f9fc1506ba0c6bc70b28137c10829
aiyl <38548129+aiyl@users.noreply.github.com>
2019-12-02 00:19:53 +1000

    add double

--- Lexer2/src/Lexer.java
+++ Lexer2/src/Lexer.java
@@ -2,17 +2,49 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 
 public class Lexer {
     String str;
     int pos;
-    FileReader fileReader;
+    HashSet<String> keyWords = new HashSet<>();
+
+    public void setKeyWords(HashSet<String> keyWords) {
+        this.keyWords = keyWords;
+        keyWords.add("begin");      keyWords.add("const");      keyWords.add("file");
+        keyWords.add("end");        keyWords.add("div");        keyWords.add("for");
+        keyWords.add("array");      keyWords.add("do");         keyWords.add("function");
+        keyWords.add("and");        keyWords.add("downto");     keyWords.add("goto");
+        keyWords.add("case");       keyWords.add("else");       keyWords.add("to");
+        keyWords.add("if");         keyWords.add("mod");        keyWords.add("of");
+        keyWords.add("in");         keyWords.add("nil");        keyWords.add("packed");
+        keyWords.add("label");      keyWords.add("not");        keyWords.add("procedure");
+        keyWords.add("record");     keyWords.add("then");       keyWords.add("var");
+        keyWords.add("repeat");     keyWords.add("type");       keyWords.add("while");
+        keyWords.add("set");        keyWords.add("until");      keyWords.add("with");
+    }
+    HashSet <Character> symbols = new HashSet<>();
+    public void setSymbols(HashSet<Character> symbols) {
+        this.symbols = symbols;
+        symbols.add('>');   symbols.add('#');   symbols.add('$');
+        symbols.add('<');   symbols.add('=');   symbols.add('&');   symbols.add('@');
+        symbols.add('^');   symbols.add('_');   symbols.add('~');   symbols.add('%');
+    }
+    HashSet <Character> arithmeticOperator = new HashSet<Character>();
+
+    public HashSet<Character> setArithmeticOperator() {
+        arithmeticOperator.add('*');    arithmeticOperator.add('/');
+        arithmeticOperator.add('-');    arithmeticOperator.add('+');
+        return arithmeticOperator;
+    }
 
     Lexer(String string) {
         this.str = string;
-
     }
 
+    public int getPos() {
+        return pos;
+    }
 
     Token next() {
         int length = str.length();
@@ -26,14 +58,25 @@ public class Lexer {
         }
         if (pos >= length)
             return null;
-
-        if (Character.isDigit(ch)) {
-            String t = "";
-            while (pos < length && Character.isDigit(str.charAt(pos))) {
+        boolean chIsDouble=false;
+        if (Character.isDigit(ch) ||(ch=='-')) {
+            String t="-";
+            if(ch=='-'){
+                pos++;
+            }
+            while (pos < length && (Character.isDigit(str.charAt(pos) )|| (str.charAt(pos)=='.'))) {
+                if (str.charAt(pos)=='.'){
+                    chIsDouble=true;
+                }
                 t += str.charAt(pos);
                 pos++;
             }
-            return new Token("int", t);
+            if(chIsDouble==true &&(str.charAt(pos-1)!='-')){
+                return new Token("double", t);
+            }
+            if (str.charAt(pos-1)!='-') {
+                return new Token("int", t);
+            }
         }
         if (Character.isLetter(ch)) {
             String t = "";
@@ -41,19 +84,16 @@ public class Lexer {
                 t += str.charAt(pos);
                 pos++;
             }
+            if(keyWords.contains(t)){
+                return new Token("Key word",t);
+            }
             return new Token("ident", t);
         }
 
-        return null;
 
-
-
-
-
-
-
-
-
+        pos++;
+        return new Token("error", String.valueOf(str.charAt(pos-1)));
+        //return null;
     }
 }
 
--- Lexer2/src/Main.java
+++ Lexer2/src/Main.java
@@ -1,9 +1,28 @@
 import java.io.*;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        int ch;
-        String line1 = "";
+        /*HashSet<String> keyWords = new HashSet<>();
+        keyWords.add("begin");      keyWords.add("const");      keyWords.add("file");
+        keyWords.add("end");        keyWords.add("div");        keyWords.add("for");
+        keyWords.add("array");      keyWords.add("do");         keyWords.add("function");
+        keyWords.add("and");        keyWords.add("downto");     keyWords.add("goto");
+        keyWords.add("case");       keyWords.add("else");       keyWords.add("to");
+        keyWords.add("if");         keyWords.add("mod");        keyWords.add("of");
+        keyWords.add("in");         keyWords.add("nil");        keyWords.add("packed");
+        keyWords.add("label");      keyWords.add("not");        keyWords.add("procedure");
+        keyWords.add("record");     keyWords.add("then");       keyWords.add("var");
+        keyWords.add("repeat");     keyWords.add("type");       keyWords.add("while");
+        keyWords.add("set");        keyWords.add("until");      keyWords.add("with");
+        HashSet <Character> arithmeticOperator = new HashSet<Character>();
+        arithmeticOperator.add('*');    arithmeticOperator.add('/');
+        arithmeticOperator.add('-');    arithmeticOperator.add('+');
+        HashSet <Character> symbols = new HashSet<>();
+        symbols.add('>');   symbols.add('#');   symbols.add('$');
+        symbols.add('<');   symbols.add('=');   symbols.add('&');   symbols.add('@');
+        symbols.add('^');   symbols.add('_');   symbols.add('~');   symbols.add('%');
+        */
         try {
             File file = new File("D:\\file.txt");
             //создаем объект FileReader для объекта File
@@ -13,15 +32,16 @@ public class Main {
             // считаем сначала первую строку
             String line = reader.readLine();
             while (line != null) {
-                Lexer lexer = new Lexer(line);
-                Token t = lexer.next();
-                if (t == null)
-                    break;
-                t.print();
-                //System.out.println(line);
+                 Lexer lexer = new Lexer(line);
+                 while (true){
+                    Token t = lexer.next();
+                    if (t == null)
+                        break;
+                    t.print();
+                }
+                    //System.out.println(line);
                 line = reader.readLine();
-                // считываем остальные строки в цикле
-
+                    // считываем остальные строки в цикле
                 //line = reader.readLine();
             }
 
@@ -31,7 +51,8 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        /*String str = "8959fhhe54 222    lslkzcv";
+
+    /*    String str = "g 22   ";
         Lexer lexer = new Lexer(str);
         while (true){
             Token t=lexer.next();
